256f61eebbbf99a964ca878868881a8874fcbeda
Misha Pavlov <misha_pavlov_01@mail.ru>
2019-10-07 21:45:18 +0300

    moving luchiki of sun and trees are made

--- a.py
+++ a.py
@@ -1,6 +1,6 @@
 from graphics import *
 from math import *
-win= GraphWin("Food", 3000, 3000)
+win= GraphWin("Food", 1000, 500)
 
 def sky():
     sk=Rectangle(Point(0,0),Point(1500,1500))
@@ -9,19 +9,23 @@ def sky():
 
 def house(x,y,n,beta,c_wall,c_roof,window_is_on_the_left,c_window,c_door,c_chimney,smoke_is_here,c_smoke,c_ruchka):
 
-    wall=Polygon(Point(x+10*n*cos(beta)-10*n*sin(beta), y+10*n*sin(beta)+10*n*cos(beta)), Point(x-10*n*cos(beta)-10*n*sin(beta),y-10*n*sin(beta)+10*n*cos(beta)), Point(x-10*n*cos(beta)+10*n*sin(beta),y-10*n*sin(beta)-10*n*cos(beta)), Point(x+10*n*cos(beta)+10*n*sin(beta), y+10*n*sin(beta)-10*n*cos(beta)))
+    wall=Polygon(Point(x+10*n*cos(beta)-10*n*sin(beta), y+10*n*sin(beta)+10*n*cos(beta)), Point(x-10*n*cos(beta)-10*n*sin(beta)
+            ,y-10*n*sin(beta)+10*n*cos(beta)), Point(x-10*n*cos(beta)+10*n*sin(beta),y-10*n*sin(beta)-10*n*cos(beta)), Point(x+10*n*cos(beta)+10*n*sin(beta), y+10*n*sin(beta)-10*n*cos(beta)))
     wall.setFill(c_wall)
     wall.draw(win)
 
-    roof=Polygon(Point(x-15*n*cos(beta)+10*n*sin(beta),y-15*n*sin(beta)-10*n*cos(beta) ), Point(x+15*n*cos(beta)+10*n*sin(beta), y+15*n*sin(beta)-10*n*cos(beta)), Point(x-0*n*cos(beta)+20*n*sin(beta),y-0*n*sin(beta)-20*n*cos(beta)))
+    roof=Polygon(Point(x-15*n*cos(beta)+10*n*sin(beta),y-15*n*sin(beta)-10*n*cos(beta) ), Point(x+15*n*cos(beta)+10*n*sin(beta),
+            y+15*n*sin(beta)-10*n*cos(beta)), Point(x-0*n*cos(beta)+20*n*sin(beta),y-0*n*sin(beta)-20*n*cos(beta)))
     roof.setFill(c_roof)
     roof.draw(win)
     
     
     if window_is_on_the_left==True:
-        window = Polygon(Point(x-8*n*cos(beta)+8*n*sin(beta),y-8*n*sin(beta)-8*n*cos(beta)), Point(x-2*n*cos(beta)+8*n*sin(beta),y-2*n*sin(beta)-8*n*cos(beta)),Point(x-2*n*cos(beta)+2*n*sin(beta),y-2*n*sin(beta)-2*n*cos(beta)),Point(x-8*n*cos(beta)+2*n*sin(beta),y-8*n*sin(beta)-2*n*cos(beta)))
+        window = Polygon(Point(x-8*n*cos(beta)+8*n*sin(beta),y-8*n*sin(beta)-8*n*cos(beta)), Point(x-2*n*cos(beta)+8*n*sin(beta),
+                y-2*n*sin(beta)-8*n*cos(beta)),Point(x-2*n*cos(beta)+2*n*sin(beta),y-2*n*sin(beta)-2*n*cos(beta)),Point(x-8*n*cos(beta)+2*n*sin(beta),y-8*n*sin(beta)-2*n*cos(beta)))
     else:
-        window = Polygon(Point(x+8*n*cos(beta)+8*n*sin(beta),y+8*n*sin(beta)-8*n*cos(beta)), Point(x+2*n*cos(beta)+8*n*sin(beta),y+2*n*sin(beta)-8*n*cos(beta)),Point(x+2*n*cos(beta)+2*n*sin(beta),y+2*n*sin(beta)-2*n*cos(beta)),Point(x+8*n*cos(beta)+2*n*sin(beta),y+8*n*sin(beta)-2*n*cos(beta)))
+        window = Polygon(Point(x+8*n*cos(beta)+8*n*sin(beta),y+8*n*sin(beta)-8*n*cos(beta)),
+        Point(x+2*n*cos(beta)+8*n*sin(beta),y+2*n*sin(beta)-8*n*cos(beta)),Point(x+2*n*cos(beta)+2*n*sin(beta),y+2*n*sin(beta)-2*n*cos(beta)),Point(x+8*n*cos(beta)+2*n*sin(beta),y+8*n*sin(beta)-2*n*cos(beta)))
     
     window.setFill(c_window)
     window.draw(win)
@@ -37,15 +41,22 @@ def house(x,y,n,beta,c_wall,c_roof,window_is_on_the_left,c_window,c_door,c_chimn
         l2=Line(Point(x+8*n*cos(beta)+5*n*sin(beta),y+8*n*sin(beta)-5*n*cos(beta)), Point(x+2*n*cos(beta)+5*n*sin(beta),y+2*n*sin(beta)-5*n*cos(beta)))
         l2.draw(win)
     if window_is_on_the_left==True:
-        door = Polygon(Point(x+0*n*cos(beta)+8*n*sin(beta),y+0*n*sin(beta)-8*n*cos(beta)),Point(x+8*n*cos(beta)+8*n*sin(beta),y+8*n*sin(beta)-8*n*cos(beta)), Point(x+8*n*cos(beta)-8*n*sin(beta),y+8*n*sin(beta)+8*n*cos(beta)),Point(x+0*n*cos(beta)-8*n*sin(beta),y-0*n*sin(beta)+8*n*cos(beta)))
+        door = Polygon(Point(x+0*n*cos(beta)+8*n*sin(beta),y+0*n*sin(beta)-8*n*cos(beta)),Point(x+8*n*cos(beta)+8*n*sin(beta),y+8*n*sin(beta)-8*n*cos(beta)), Point(x+8*n*cos(beta)-8*n*sin(beta),
+                    y+8*n*sin(beta)+8*n*cos(beta)),Point(x+0*n*cos(beta)-8*n*sin(beta),y-0*n*sin(beta)+8*n*cos(beta)))
         door.setFill(c_door)
         door.draw(win)
     else:
-        door = Polygon(Point(x+0*n*cos(beta)+8*n*sin(beta),y+0*n*sin(beta)-8*n*cos(beta)),Point(x-8*n*cos(beta)+8*n*sin(beta),y-8*n*sin(beta)-8*n*cos(beta)), Point(x-8*n*cos(beta)-8*n*sin(beta),y-8*n*sin(beta)+8*n*cos(beta)),Point(x+0*n*cos(beta)-8*n*sin(beta),y-0*n*sin(beta)+8*n*cos(beta)))
+        door = Polygon(Point(x+0*n*cos(beta)+8*n*sin(beta),y+0*n*sin(beta)-8*n*cos(beta)),
+        Point(x-8*n*cos(beta)+8*n*sin(beta),y-8*n*sin(beta)-8*n*cos(beta)), Point(x-8*n*cos(beta)-8*n*sin(beta),
+                y-8*n*sin(beta)+8*n*cos(beta)),Point(x+0*n*cos(beta)-8*n*sin(beta),y-0*n*sin(beta)+8*n*cos(beta)))
         door.setFill(c_door)
         door.draw(win)
     
-    chimney=Polygon(Point(x-10*n*cos(beta)+13*n*sin(beta),y-10*n*sin(beta)-13*n*cos(beta)),Point(x-10*n*cos(beta)+17*n*sin(beta),y-10*n*sin(beta)-17*n*cos(beta)),Point(x-12*n*cos(beta)+17*n*sin(beta),y-12*n*sin(beta)-17*n*cos(beta)),Point(x-12*n*cos(beta)+19*n*sin(beta),y-12*n*sin(beta)-19*n*cos(beta)),Point(x-4*n*cos(beta)+19*n*sin(beta),y-4*n*sin(beta)-19*n*cos(beta)),Point(x-4*n*cos(beta)+17*n*sin(beta),y-4*n*sin(beta)-17*n*cos(beta)),Point(x-6*n*cos(beta)+17*n*sin(beta),y-6*n*sin(beta)-17*n*cos(beta)),Point(x-6*n*cos(beta)+16*n*sin(beta),y-6*n*sin(beta)-16*n*cos(beta)),Point(x-10*n*cos(beta)+13*n*sin(beta),y-10*n*sin(beta)-13*n*cos(beta)))
+    chimney=Polygon(Point(x-10*n*cos(beta)+13*n*sin(beta),y-10*n*sin(beta)-13*n*cos(beta)),Point(x-10*n*cos(beta)+17*n*sin(beta),
+      y-10*n*sin(beta)-17*n*cos(beta)),Point(x-12*n*cos(beta)+17*n*sin(beta),y-12*n*sin(beta)-17*n*cos(beta)),Point(x-12*n*cos(beta)+19*n*sin(beta),
+            y-12*n*sin(beta)-19*n*cos(beta)),Point(x-4*n*cos(beta)+19*n*sin(beta),y-4*n*sin(beta)-19*n*cos(beta)),Point(x-4*n*cos(beta)+17*n*sin(beta),
+                y-4*n*sin(beta)-17*n*cos(beta)),Point(x-6*n*cos(beta)+17*n*sin(beta),y-6*n*sin(beta)-17*n*cos(beta)),Point(x-6*n*cos(beta)+16*n*sin(beta),
+                    y-6*n*sin(beta)-16*n*cos(beta)),Point(x-10*n*cos(beta)+13*n*sin(beta),y-10*n*sin(beta)-13*n*cos(beta)))
     chimney.setFill(c_chimney)
     chimney.draw(win)
     
@@ -68,30 +79,64 @@ def house(x,y,n,beta,c_wall,c_roof,window_is_on_the_left,c_window,c_door,c_chimn
         
 
 def sun(n,x,y,r,c_sun):
-    sun = Circle(Point(x,y), r)
+    sun = Circle(Point(x, y), r)
     sun.setFill(c_sun)
     sun.setOutline(c_sun)
     sun.draw(win)
+
+def luchiki(n,x,y,r,c_sun):
+    v=(5,5)
+    k=[1,1,1,1,1,1,1,1,1,1]
+    g=[1,1,1,1,1,1,1,1,1,1]
     for i in range(n):
-        z = x + int(100*(sin(2*i*pi/n)))
-        d = y + int(100*(cos(2*i*pi/n)))
-        l=Line(Point(x, y), Point(z,d))
-        l.setWidth(10)
+        z = x + 100*(sin(2*i*pi/n))
+        d = y + 100*(cos(2*i*pi/n))
+        l = Line(Point(x, y), Point(z,d))
+        l.setWidth(4)
         l.setFill(c_sun)
-        l.draw(win)
+        k[i] = l
+
+    for i in range(n):
+         z = x + 100 * (sin(2 * i * pi / n + pi/n))
+         d = y + 100 * (cos(2 * i * pi / n + pi/n))
+         l = Line(Point(x, y), Point(z, d))
+         l.setWidth(4)
+         l.setFill(c_sun)
+         g[i] = l
+
+    for i in range(n):
+        k[i].draw(win)
+    for i in range(n):
+        k[i].undraw()
+        g[i].draw(win)
+        time.sleep(0.01)
+    for i in range(n):
+        g[i].undraw()
+    time.sleep(0.001)
+
+
+
+
+def add(point_1,point_2):
+    new_point=Point(point_1.x+point_2.x,point_1.y+point_2.y)
+    return(new_point)
         
  
 
 
+
+def tree(r,c_krona,c_trunk,coords1_x,coords1_y):
     
-def tree(x,y,r,c_krona,c_trunk):
-    
-    trunk=Rectangle(Point(x-r/4,y+r/2),Point(x+r/4,y+3*r))
+    trunk=Rectangle(Point(coords1_x-r/4,coords1_y+r/2),Point(coords1_x+r/4,coords1_y+3*r))
     trunk.setFill(c_trunk)
     trunk.draw(win)
-    krona=Circle(Point(x,y), r)
+
+    krona=Circle(Point(coords1_x,coords1_y), r)
     krona.setFill(c_krona)
     krona.draw(win)
+
+    return(trunk,krona,r,coords1_x,coords1_y)
+
 def human(x,y,r,c_head,c_telo,c_nogi,c_ruki,c_eyes):
    
     
@@ -132,16 +177,61 @@ def oblako(x,y,r,c_oblako):
     o.setFill(c_oblako)
     o.setOutline(c_oblako)
     o.draw(win)
+
+
+
 sky()
 grass = Polygon(Point(1000,450), Point(0,450), Point(0,500), Point(1000,500))
 grass.setFill('green')
 grass.draw(win)
-sun(20,100,150,50,'violet') 
-house(900,450,6,pi/2,'grey','red',True,'blue','violet','red',True,'white','blue')
-tree(300,300,100,'green','black')
-human(200,200,40,'blue','red','green','yellow','white')
+
+
+
+
+house(x=800,y=250,n=10,beta=pi,c_wall='grey',c_roof='red',window_is_on_the_left=True,c_window='blue',c_door='brown',c_chimney='red',smoke_is_here=False,c_smoke='white',c_ruchka='blue')
+
+human(500,400,20,'blue','red','green','yellow','white')
 for i in range(4):
     oblako(10+100*i,100+20*i,10,'white')
+    oblako(400+100*i,100+20*i,10,'white')
+    z=Point(20,20)
+
+    #начало алгоритма движения деревьев
+a=[1,1,1]
+b=[1,1,1]
+coords_x=[1,1,1]
+coords_y=[1,1,1]
+for i in range(2):
+    a[i], b[i], r, coords_x[i],coords_y[i] = tree( r=20, c_krona='green', c_trunk='yellow', coords1_x=300+100*i, coords1_y=400+100*i)
+
+
+v_x=[5,5,5]  #на i-том месте стоит проекция скорости i-ого дерева
+v_y=[5,5,5]  #на i-том месте стоит проекция скорости i-ого дерева
+def check_coords(i):
+    if coords_x[i]-r<0 or coords_x[i]+r>1000:
+        v_x[i]=-v_x[i]
+    if (coords_y[i]+3*r < 455 and v_y[i]<0)  or (coords_y[i]+3*r > 500 and v_y[i]>0):
+        v_y[i] = -v_y[i]
+    return(v_x[i],v_y[i])
+
+sun(10,100,150,50,'violet')
+while  True:
+    for i in range(2):
+        check_coords(i)
+        a[i].move(v_x[i], v_y[i])
+        b[i].move(v_x[i], v_y[i])
+        coords_x[i] += v_x[i]
+        coords_y[i] += v_y[i]
+    luchiki(10, 100, 150, 50, 'violet')
+    time.sleep(0.001)
+
+
+
+
+
+
+
+
 win.getMouse()
 win.close()
 
